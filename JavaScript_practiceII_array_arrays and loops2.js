84ec2e2ab1e0be6765d9bf99306d10d863eab5e3
riceshower001 <125946802+riceshower001@users.noreply.github.com>
2023-02-27 03:23:05 +0900

    Add files via upload

---
+++ JavaScript_practiceII_array_arrays and loops2.js
@@ -0,0 +1,11 @@
+// Let's use length.
+
+const animals = ["dog", "cat", "sheep", "rabbit", "monkey", "tiger", "bear", "elephant"];
+
+// Please use length to print the number of elements in the array.
+console.log(animals.length);
+
+// Please rewrite the conditional expression using length.
+for (let i = 0; i < animals.length; i++) {
+  console.log(animals[i]);
+}
